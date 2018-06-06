9a631aaf62c1329d58346fa1035835b2b5333a67
Johannes Sahlmann <jsahlmann@stsci.edu>
2018-06-06 08:31:28 -0400

    Remove test import

--- pysiaf/__init__.py
+++ pysiaf/__init__.py
@@ -11,7 +11,7 @@ from .constants import JWST_PRD_VERSION, JWST_PRD_DATA_ROOT, JWST_PRD_DATA_ROOT_
     HST_PRD_DATA_ROOT
 from .iando import read, write
 from .siaf import Siaf, ApertureCollection
-from .tests import test_aperture#, test_polynomial
-from .utils import polynomial, rotations, tools#, tools2
+# from .tests import test_aperture#, test_polynomial
+from .utils import polynomial, rotations, tools
 
 __all__ = ['Aperture', 'HstAperture', 'JwstAperture', 'SIAF', 'JWST_PRD_VERSION', 'JWST_PRD_DATA_ROOT', 'HST_PRD_VERSION', 'HST_PRD_DATA_ROOT', '_JWST_STAGING_ROOT', 'siaf', 'iando', 'polynomial', 'rotations', 'tools', 'compare', 'JWST_PRD_DATA_ROOT_EXCEL', 'generate', 'tools2']
