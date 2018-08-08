76d418681c3a51be4ca6855ef46a349ef974bdf0
Colin Cox <cox@stsci.edu>
2018-08-08 11:52:37 -0400

    Added the method set_polynomial_coefficients to Aperture object

--- pysiaf/tests/test_match_v2v3.py
+++ pysiaf/tests/test_match_v2v3.py
@@ -11,4 +11,6 @@ siaf = iando.read.read_jwst_siaf(instrument=instrument)
 aperture_1 = siaf[apName_1]
 aperture_2 = siaf[apName_2]
 
-tools.match_v2v3(aperture_1, aperture_2, verbose=True)
+new_aperture_2 = tools.match_v2v3(aperture_1, aperture_2, verbose=True)
+assert new_aperture_2.V2Ref == aperture_1.V2Ref
+assert new_aperture_2.V3Ref == aperture_1.V3Ref
