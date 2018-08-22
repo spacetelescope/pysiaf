ff4917838134b6766481fc9b1693ea26313752bb
Colin Cox <cox@stsci.edu>
2018-08-22 10:05:02 -0400

    Renamed test. Added new line indicated byPEP

--- pysiaf/tests/test_match_v2v3.py
+++ pysiaf/tests/test_match_v2v3.py
@@ -1,7 +1,8 @@
 from pysiaf import iando
 from pysiaf.utils import tools
 
-def testit(instrument='NIRCam', apName_1 = 'NRCA1_FULL', apName_2 = 'NRCA5_FULL', verbose=False):
+
+def test_match_v2v3(instrument='NIRCam', apName_1='NRCA1_FULL', apName_2='NRCA5_FULL', verbose=False):
     siaf = iando.read.read_jwst_siaf(instrument=instrument)
     aperture_1 = siaf[apName_1]
     aperture_2 = siaf[apName_2]
