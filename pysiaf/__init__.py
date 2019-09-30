1620ce5050961b62cbcf4aa062c996da8da137e1
Shannon Osborne <sosborne@stsci.edu>
2019-09-30 10:39:06 -0400

    Catch ConnectionError and make warning more bold

--- pysiaf/__init__.py
+++ pysiaf/__init__.py
@@ -20,11 +20,15 @@ from .utils import polynomial, rotations, tools, projection
 __all__ = ['Aperture', 'HstAperture', 'JwstAperture', 'SIAF', 'JWST_PRD_VERSION', 'JWST_PRD_DATA_ROOT', 'HST_PRD_VERSION', 'HST_PRD_DATA_ROOT', '_JWST_STAGING_ROOT', 'siaf', 'iando', 'polynomial', 'rotations', 'tools', 'compare', 'JWST_PRD_DATA_ROOT_EXCEL', 'generate', 'projection']
 
 # Check PRD version is up to date
-req = requests.get('https://github.com/spacetelescope/pysiaf/tree/master/pysiaf/prd_data/JWST').text
-p = re.compile("/spacetelescope/pysiaf/tree/master/pysiaf/prd_data/JWST/(.*?)/SIAFXML")
-prd_list = p.findall(req)
-newest_prd = [x for x in sorted(prd_list, reverse=True)][0]
+try:
+    req = requests.get('https://github.com/spacetelescope/pysiaf/tree/master/pysiaf/prd_data/JWST').text
+    p = re.compile("/spacetelescope/pysiaf/tree/master/pysiaf/prd_data/JWST/(.*?)/SIAFXML")
+    prd_list = p.findall(req)
+    newest_prd = [x for x in sorted(prd_list, reverse=True)][0]
 
-if JWST_PRD_VERSION != newest_prd:
-    print("Warning: Local JWST PRD version {} is behind the current online version {}".format(
-        JWST_PRD_VERSION, newest_prd))
+    if JWST_PRD_VERSION != newest_prd:
+        print("**WARNING**: LOCAL JWST PRD VERSION {} IS BEHIND THE CURRENT ONLINE VERSION {}\nPlease "
+              "consider updating pysiaf, e.g. pip install --upgrade pysiaf or conda update pysiaf".format(
+              JWST_PRD_VERSION, newest_prd))
+except requests.exceptions.ConnectionError:
+    pass
