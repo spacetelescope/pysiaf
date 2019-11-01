b73a8289c0aa950d6e605884fc881757d5de1ad1
Mario Gennaro <gennaro@hari-seldon.stsci.edu>
2019-11-01 15:08:34 -0400

    Changed values for all NIRCam coronographic apertures

--- generate/generate_nircam.py
+++ generate/generate_nircam.py
@@ -351,7 +351,7 @@ if emulate_delivery:
     compare_against_prd = True
     compare_against_cdp7b = True
 
-    for compare_to in [pysiaf.JWST_PRD_VERSION, 'outdated pre-delivery']:
+    for compare_to in [pysiaf.JWST_PRD_VERSION]:
         if compare_to == 'outdated pre-delivery':
             ref_siaf = pysiaf.Siaf(instrument, filename=os.path.join(pre_delivery_dir, 'NIRCam_SIAF_outdated.xml'))
         else:
@@ -369,7 +369,7 @@ if emulate_delivery:
         compare.compare_inspection_figures(pre_delivery_siaf, reference_siaf_input=ref_siaf,
                                            report_dir=pre_delivery_dir, tags=tags, mark_ref=True)
 
-        create_jira_plots = False
+        create_jira_plots = True
         if create_jira_plots:
             # make figures for JWSTSIAF-129 Jira ticket
             # selected_aperture_names = [['NRCA1_GRISMTS', 'NRCA5_GRISM_F444W'],
@@ -393,7 +393,13 @@ if emulate_delivery:
                                        ['NRCA5_FSTAMASKLWB', 'NRCA5_FULL_FSTAMASKLWB'],
                                        ['NRCA5_FSTAMASK335R', 'NRCA5_FULL_FSTAMASK335R'],
                                        ['NRCA5_FSTAMASK430R', 'NRCA5_FULL_FSTAMASK430R'],
-                                       ]
+                                       
+#                                       ['NRCA2_MASK210R','NRCA5_MASK335R','NRCA5_MASK430R','NRCA4_MASKSWB','NRCA5_MASKLWB'],
+
+                                       ['NRCA2_MASK210R','NRCA5_MASK335R','NRCA5_MASK430R','NRCA4_MASKSWB','NRCA5_MASKLWB',
+                                       'NRCA2_TAMASK210R','NRCA5_TAMASK335R','NRCA5_TAMASK430R','NRCA4_TAMASKSWB','NRCA5_TAMASKLWB', 'NRCA5_TAMASKLWBL','NRCA4_TAMASKSWBS',
+                                       'NRCA2_FSTAMASK210R','NRCA4_FSTAMASKSWB','NRCA5_FSTAMASKLWB','NRCA5_FSTAMASK335R','NRCA5_FSTAMASK430R']
+                                      ]
 
             for selected_aperture_name in selected_aperture_names:
                 compare.compare_inspection_figures(pre_delivery_siaf, reference_siaf_input=ref_siaf,
