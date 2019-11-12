df4a1508a0d8d9855c1ec009b7d6e179b007c1cb
Shannon Osborne <sosborne@stsci.edu>
2019-11-12 16:24:36 -0500

    Add basic tests for writing JWST SIAF XML and XLSX files

---
+++ pysiaf/tests/test_iando.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""Tests for the pysiaf iando/ write.py and read.py functions.
+
+Authors
+-------
+
+    Shannon Osborne
+
+"""
+import os
+
+import pytest
+
+from ..aperture import JwstAperture
+from ..siaf import ApertureCollection
+from ..iando.write import write_jwst_siaf
+
+
+def test_write_jwst_siaf_xml(tmpdir):
+    """Basic test to check that JWST SIAF XML file is written out"""
+
+    aperture = JwstAperture()
+    aperture.AperName = 'MIRIM_FULL_OSS'
+    aperture.InstrName = 'MIRI'
+    aperture.VIdlParity = 1
+    aperture.DetSciYAngle = 0
+    aperture.DetSciParity = 1
+    aperture_dict = {
+        'MIRIM_FULL_OSS': aperture
+    }
+    aperture_collection = ApertureCollection(aperture_dict)
+    filename = os.path.join(tmpdir, 'test_miri.xml')
+
+    write_jwst_siaf(aperture_collection, filename=filename,
+                    file_format='xml', verbose=False)
+
+    assert os.path.isfile(filename)
+
+    # Remove temporary directory
+    tmpdir.remove()
+
+
+def test_write_jwst_siaf_xlsx(tmpdir):
+    """Basic test to check that JWST SIAF XLSX file is written out"""
+
+    aperture = JwstAperture()
+    aperture.AperName = 'MIRIM_FULL_OSS'
+    aperture.InstrName = 'MIRI'
+    aperture.VIdlParity = 1
+    aperture.DetSciYAngle = 0
+    aperture.DetSciParity = 1
+    aperture_dict = {
+        'MIRIM_FULL_OSS': aperture
+    }
+    aperture_collection = ApertureCollection(aperture_dict)
+    filename = os.path.join(tmpdir, 'test_miri.xlsx')
+
+    write_jwst_siaf(aperture_collection, filename=filename,
+                    file_format='xlsx', verbose=False)
+
+    assert os.path.isfile(filename)
+
+    # Remove temporary directory
+    tmpdir.remove()
