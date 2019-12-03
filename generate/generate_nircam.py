d564883651df392a6ffb8dfe9b487d5ba69a767e
Mario Gennaro <gennaro@hari-seldon.stsci.edu>
2019-12-03 16:02:59 -0500

    Moved the bit of code that creates the enhanced aperture definition file into its own module under pysiaf/utils, named enhanced_aperture_file. Modified genereate_nircam accordingly to use such function. The function is called within the emulate_delivery clause of generate_nircam

--- generate/generate_nircam.py
+++ generate/generate_nircam.py
@@ -24,6 +24,8 @@ import matplotlib.pyplot as pl
 
 import pysiaf
 from pysiaf.utils import tools, compare
+from pysiaf.utils.enhanced_aperture_file import create_enhanced_aperture_file
+
 from pysiaf.constants import JWST_SOURCE_DATA_ROOT, JWST_TEMPORARY_DATA_ROOT, \
     JWST_DELIVERY_DATA_ROOT
 from pysiaf import iando
@@ -385,6 +387,12 @@ if emulate_delivery:
                                                    label=True)
                 pl.close('all')  # stops system from being overwhelmed with too may plots
 
+    #If desired, create the enhanced aperture file containing OSS corners as well as V2/V3 positions of the reference point
+    enhanced_aperture_file =  True
+    
+    if enhanced_aperture_file:
+        create_enhanced_aperture_file(aperture_dict)    
+
     # run some tests on the new SIAF
     from pysiaf.tests import test_aperture
     print('\nRunning aperture_transforms test for pre_delivery_siaf')
@@ -405,75 +413,3 @@ else:
     ignore_attributes = 'XIdlVert1 XIdlVert2 XIdlVert3 XIdlVert4 YIdlVert1 YIdlVert2 YIdlVert3 YIdlVert4'.split()
     ignore_attributes += ([s for s in DISTORTION_ATTRIBUTES if 'Idl2Sci' in s])
     compare.compare_siaf(new_siaf, reference_siaf_input=ref_siaf, fractional_tolerance=1e-6, ignore_attributes=ignore_attributes)
-
-
-######################################
-# Print corners
-######################################
-
-in_file = os.path.join(JWST_SOURCE_DATA_ROOT,instrument,'nircam_siaf_aperture_definition.txt')
-out_file = os.path.join(JWST_DELIVERY_DATA_ROOT, instrument,'nircam_enhanced_siaf_aperture_definition.txt')
-
-print('Writing SIAF aperture definition file including subarrays and V2/V3 of reference point')
-with open(in_file) as fp:
-    lines = fp.readlines()
-
-with open(out_file, 'w') as the_file:
-    
-    for line in lines:
-        
-        newline = line[:]
-
-        # If the original siaf file line starts with a comment or with a newline command (i.e. empty line), do not change it
-        if (line[0] in ['#','\n']) == False:
-            llist = line.split(',')
-            AperName = llist[0].strip()
-
-            # The header line does not start with "#" neither it is empty. Yet we must add two header columns. 
-            # The try statement fails because the first element oin the header row is not a valid aperture name, the exception is captured,
-            # the header modified an the continue statment breaks this iteration of the for loop on the lines
-            
-            try:
-                ap = aperture_dict[AperName]
-                
-            except:
-                llist.insert(8,' {:>11} '.format('ColCorner'))
-                llist.insert(9,' {:>11} '.format('RowCorner'))
-                llist.insert(10,' {:>20} '.format('V2_ref'))
-                llist.insert(11,' {:>20} '.format('V3_ref'))
-                
-                newline = ','.join(llist)
-                the_file.write(newline)
-                continue
-            
-            else:
-                # If instead the try statement does not fail, execute this block
-                # This second try/except statement is necessary to capture the exception thrown by calling the corner method on apertures that do not have detector corners,      
-                # like the compoud and  grism_wfss ones. If the try fails, we add "None" for the OSS corner, otherwise we use the new DMS_corner method of the aperture object
-                try:
-                    ColCorner, RowCorner = ap.DMS_corner()
-                       
-                except:
-                    llist.insert(8,' {:>11} '.format('None'))
-                    llist.insert(9,' {:>11} '.format('None'))
-                else:            
-                    llist.insert(8,' {:>11} '.format(ColCorner))
-                    llist.insert(9,' {:>11} '.format(RowCorner))
-
-                # This third try/except statement is necessary to capture the exception thrown by calling the reference point method on apertures that do not have one.      
-                # If the try fails, we add "None" for the V2/V3 ref, otherwise we use the reference_point('idl') method of the aperture object
-                try:
-                    V2_ref, V3_ref = ap.reference_point('tel')
-                       
-                except:
-                    llist.insert(10,' {:>20} '.format('None'))
-                    llist.insert(11,' {:>20} '.format('None'))
-                else:            
-                    llist.insert(10,' {:20.14f} '.format(V2_ref))
-                    llist.insert(11,' {:20.14f} '.format(V3_ref))
-
-            newline = ','.join(llist)
-            print(newline)
-
-        the_file.write(newline)
-
